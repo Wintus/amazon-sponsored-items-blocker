ae3dc3390dffb93473779d71a0cd9ba5b3bf2983
Yuya Ma'emichi <6386129+Wintus@users.noreply.github.com>
2021-01-18 00:01:05 +0900

    Reformat by Prettier

--- amazon-sponsored-items-blocker.user.js
+++ amazon-sponsored-items-blocker.user.js
@@ -11,53 +11,58 @@
 
 // using: ES2015
 
-const label = 'amazon-sponsored-items-blocker';
-const mainId = 'search';
-const listClass = 's-main-slot';
-const adClass = 'AdHolder'; // direct child of the above
+const label = "amazon-sponsored-items-blocker";
+const mainId = "search";
+const listClass = "s-main-slot";
+const adClass = "AdHolder"; // direct child of the above
 
 /**
  * @param {Node} node
  * @return {boolean} node is HTMLElement
  */
 const isAd = (node) =>
-    node instanceof HTMLElement &&
-    node.classList.contains(adClass);
+  node instanceof HTMLElement && node.classList.contains(adClass);
 
 /**
  * @param {Node[]} nodes
  */
-const removeSponsoredAds = nodes => {
-    /**
-     * @type {HTMLElement[]}
-     */
-    const ads = nodes.filter(isAd);
-    console.debug(`${label}: ads`, ads);
+const removeSponsoredAds = (nodes) => {
+  /**
+   * @type {HTMLElement[]}
+   */
+  const ads = nodes.filter(isAd);
+  console.debug(`${label}: ads`, ads);
 
-    for (const ad of ads) {
-        ad.remove();
-    }
-    console.log(`${label}: ${ads.length} ads removed!`);
+  for (const ad of ads) {
+    ad.remove();
+  }
+  console.log(`${label}: ${ads.length} ads removed!`);
 };
 
-const observer = new MutationObserver(mutations => {
-    const nodes = mutations.flatMap(({target: {childNodes}, addedNodes}) => [...childNodes, ...addedNodes]);
-    removeSponsoredAds(nodes);
+const observer = new MutationObserver((mutations) => {
+  const nodes = mutations.flatMap(({ target: { childNodes }, addedNodes }) => [
+    ...childNodes,
+    ...addedNodes,
+  ]);
+  removeSponsoredAds(nodes);
 });
 
 /**
  * @type {Element | null}
  */
-const main = document.getElementById(mainId).getElementsByClassName(listClass).item(0);
+const main = document
+  .getElementById(mainId)
+  .getElementsByClassName(listClass)
+  .item(0);
 if (main) {
-    // initial
-    removeSponsoredAds(Array.from(main.children));
-    // changes
-    observer.observe(main, {
-        childList: true,
-    });
+  // initial
+  removeSponsoredAds(Array.from(main.children));
+  // changes
+  observer.observe(main, {
+    childList: true,
+  });
 } else {
-    console.error(`${label}: no target`);
+  console.error(`${label}: no target`);
 }
 
 console.log(`${label}: loaded`);
